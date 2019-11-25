7a9102a64b7e2cb42cd89f1af3fa53ffce03d2d5
pramila <pmiris@example.com>
2019-11-25 09:01:39 -0800

    Java_Stack

---
+++ java/java_fundamentals/Ownjava.java
@@ -0,0 +1,11 @@
+public class Ownjava{
+    public static void main(String[] args) {
+        int myage = 25;
+        String myname ="Pramila";
+        String myhometown ="Nepal";
+        System.out.println("My name is " + myname);
+        System.out.println("I am " + myage + " years old");
+       
+        System.out.println("My hometown is " + myhometown);
+    }
+}
